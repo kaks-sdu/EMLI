4f5625dd113efc3a068f66ffb42d6beb26d21fb8
RPI <kristian.have@hotmail.com>
2023-05-19 12:13:12 +0200

    added tee instead of using fifo queues

---
+++ RPI/start_tee.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the list of ports
+ports=("/dev/ttyACM0")
+
+# Trap the SIGINT signal and kill all child processes
+trap cleanup SIGINT
+
+cleanup() {
+  # Kill all child processes
+  kill $(jobs -p)
+  exit
+}
+
+# Start a child process for each port
+for port in "${ports[@]}"; do
+  # Start a background process for each port
+  # Pass the port as an argument to the scripts and split the output using tee
+  ./read_serial.sh "$port" | tee >(./control_pump.sh "$port") >(./mqtt_publish.sh "$port") >/dev/null &
+done
+
+# Wait for all child processes to finish
+wait
