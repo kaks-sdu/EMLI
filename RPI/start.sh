cee83b7a50332ebd6392b72c540b47e46276b131
khave <kristian.have@hotmail.com>
2023-05-19 11:31:17 +0200

    Change start to include mqtt

--- RPI/start.sh
+++ RPI/start.sh
@@ -4,12 +4,21 @@
 ports=("/dev/ttyACM0") # ports=("/dev/ttyACM0" "/dev/ttyACM1" "/dev/ttyACM2")
 
 # Start a child process for each port
-pids=()
 for port in "${ports[@]}"; do
+    # Create a named pipe
+    mkfifo "$port.fifo"
+
     # Start a background process for each port
-    # Pass the port as an argument to both scripts and pipe the output of read_serial.sh to control_pump.sh
-    ./read_serial.sh "$port" | ./control_pump.sh "$port" &
-    pids+=($!) # Store the PID of the subprocess
+    # Pass the port as an argument to the scripts and redirect the output of read_serial.sh to the named pipe
+    ./read_serial.sh "$port" > "$port.fifo" &
+
+    # Start control_pump.sh and mqtt_start_script.sh in background
+    # Both scripts will read from the named pipe
+    ./control_pump.sh "$port" < "$port.fifo" &
+    ./mqtt_start_script.sh "$port" < "$port.fifo" &
+
+    # Remove the named pipe after use
+    rm "$port.fifo"
 done
 
 # Trap the SIGINT signal and kill child processes
