39fbce29d97ff5891d3b40d2d504c98c0f52750f
RPI <kristian.have@hotmail.com>
2023-05-25 14:04:10 +0200

    added logic to control pump

--- RPI/control_pump.sh
+++ RPI/control_pump.sh
@@ -6,8 +6,22 @@ threshold=50
 pump_duration=1
 alarm=0  # Indicator for alarm status 
 
-# Initialize the timer
+# Initialize the timer and last received pico_id
 timer_start=$(date +%s)
+last_received_pico_id=""
+
+broker="broker.hivemq.com"
+qos=1
+root="/kaks/"
+topic_start_pump="start_pump"
+
+# Start MQTT subscriber as a co-process
+coproc mqtt_subscriber {
+  mosquitto_sub -t "${root}$topic_start_pump" -h $broker -q $qos
+}
+
+# Assign the output file descriptor of the co-process to a variable
+subscriber_fd=${mqtt_subscriber[0]}
 
 while IFS=',' read -r pico_id plant_water_alarm pump_water_alarm moisture light
 do
@@ -30,11 +44,21 @@ do
     alarm=0
   fi
 
+  # Check for incoming MQTT messages
+  if read -t 1 -u "$subscriber_fd" start_pump_message; then
+    echo "Received MQTT message: $start_pump_message"
+    if [[ $start_pump_message == $pico_id ]]; then
+      echo -n 'p' > $port
+      echo "Starting pump due to MQTT message"
+      last_received_pico_id=$start_pump_message
+    fi
+  fi
+
   # Run the pump every 12 hours
   if [[ $time_elapsed -ge 43200 ]] && [[ $alarm -eq 0 ]]; then
     echo -n 'p' > $port
     timer_start=$(date +%s)
-    echo "run punp every 12h"
+    echo "run pump every 12h"
   fi
 
   # Run the pump once every hour if the soil moisture falls below a certain threshold
@@ -44,3 +68,6 @@ do
   fi
 
 done
+
+# Clean up the co-process
+kill "$mqtt_subscriber_PID"
